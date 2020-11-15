48b7f71f18ca3e6346512e0b430f3fef1fde9f62
Gallaguz <gallaguz@gmail.com>
2020-11-15 08:20:42 +0300

    просмотр списка техники (убрал поле цена, поскольку это относится к самому объявлению), просмотр списка объявлений с ценами, добавил сидер для объявлений

---
+++ database/seeders/PromosSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Promo;
+use Illuminate\Database\Seeder;
+
+class PromosSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Promo::factory()
+            ->times(100)
+            ->create();
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\Admin\AdminController;
 use App\Http\Controllers\IndexController;
 use App\Http\Controllers\ItemsController;
 use App\Http\Controllers\OrderController;
+use App\Http\Controllers\PromoController;
 use App\Http\Controllers\UsersController;
 use Illuminate\Support\Facades\Route;
 
@@ -25,14 +26,20 @@ Route::prefix('items')->as('items.')->group(function () {
     Route::get('/{items}', [ItemsController::class, 'show'])->name('show');
 });
 
+Route::prefix('promos')->as('promos.')->group(function () {
+    Route::get('/', [PromoController::class, 'index'])->name('index');
+    Route::get('/{promos}', [PromoController::class, 'show'])->name('show');
+});
+
+
+
+
 Route::get('/users', [UsersController::class, 'index'])->name('users');
 
 
 Route::get('/admin', [AdminController::class, 'index'])->name('admin');
 
-Route::get('/home', function () {
-    return view('welcome');
-});
+
 Route::get('/ads', [ItemsController::class, 'index']);
 
 Route::resource('orders', OrderController::class);
