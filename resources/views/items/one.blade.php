38f03b5647f8688bf2bbc4177c629f6a9ad425c7
pozys <pozys@mail.ru>
2020-11-19 00:45:51 +0300

    Добавил вывод фото Исправил имя атрибута, выражающего наименование техники

--- resources/views/items/one.blade.php
+++ resources/views/items/one.blade.php
@@ -3,14 +3,20 @@
 @section('title', 'Item')
 
 @section('content')
-	
-    <h1>{{ $item->title }}</h1>
-    <div>Описание: {{ $item->characteristic->description }}</div>
-    <div>Длина: {{ $item->characteristic->length }}</div>
-    <div>Высота: {{ $item->characteristic->height }}</div>
-    <div>Ширина: {{ $item->characteristic->width }}</div>   
-    <div>Масса: {{ $item->characteristic->mass }}</div>  
-    <div>Мощность (Вт): {{ $item->characteristic->power_watt }}</div> 
-    <div>Мощность (л/с): {{ $item->characteristic->horse_power }}</div>
-    <div>Грузоподъемность: {{ $item->characteristic->lifting_capacity }}</div>
+
+<h1>{{ $item->name }}</h1>
+<div>Описание: {{ $item->characteristic->description }}</div>
+<div>Длина: {{ $item->characteristic->length }}</div>
+<div>Высота: {{ $item->characteristic->height }}</div>
+<div>Ширина: {{ $item->characteristic->width }}</div>
+<div>Масса: {{ $item->characteristic->mass }}</div>
+<div>Мощность (Вт): {{ $item->characteristic->power_watt }}</div>
+<div>Мощность (л/с): {{ $item->characteristic->horse_power }}</div>
+<div>Грузоподъемность: {{ $item->characteristic->lifting_capacity }}</div>
+<div>
+    @foreach($item->medias as $media)
+    <img src="{{ asset('storage/' . $media->filename) }}" alt="none">
+    @endforeach
+</div>
+
 @endsection
