1fbd568c94f4703e065913272885acc9d4e7c7cc
pozys <pozys@mail.ru>
2020-11-18 23:39:39 +0300

    Добавил трейт для использования SoftDeletes Добавил массив fillable Добавил функцию получения медиа-файлов этого item

--- app/Models/Item.php
+++ app/Models/Item.php
@@ -4,10 +4,17 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Item extends Model
 {
     use HasFactory;
+    use SoftDeletes;
+
+    protected $fillable = [
+        'settlement_id',
+        'name'
+    ];
 
     public function characteristic()
     {
@@ -17,5 +24,10 @@ class Item extends Model
     public function tags()
     {
         return$this->belongsToMany(Tag::class);
+    }
+
+    public function medias()
+    {
+        return $this->hasMany(Media::class);
     }
 }
