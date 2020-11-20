48b6b4064d159af7c7265398879350467b5d4ffb
pozys <pozys@mail.ru>
2020-11-20 08:48:17 +0300

    Добавил функцию для сохранения связанной с Item информации

--- app/Models/Item.php
+++ app/Models/Item.php
@@ -23,7 +23,7 @@ class Item extends Model
 
     public function tags()
     {
-        return$this->belongsToMany(Tag::class);
+        return $this->belongsToMany(Tag::class);
     }
 
     public function medias()
@@ -34,5 +34,41 @@ class Item extends Model
     public function settlement()
     {
         return $this->belongsTo(Settlement::class);
+    }
+
+    public function saveRelations($parameters, $files, int $user_id = 0)
+    {
+        $isUpdate = empty($user_id);
+        if (!$isUpdate) {
+            $user = User::find($user_id);
+            $user->items()->attach($this->id);
+        }
+
+        if ($isUpdate) {
+            $this->characteristic->fill($parameters)->save();
+            $this->tags()->sync($parameters['tag_ids']);
+        } else {
+            $this->characteristic()->create($parameters);
+            
+            foreach ($parameters['tag_ids'] as $tag_id) {
+                $this->tags()->attach($tag_id);
+            }
+        }
+
+        if ($files) {
+            foreach ($files as $images) {
+                foreach ($images as $image) {
+                    $path = $image->store('items_files');
+                    $media = new Media();
+                    $media->item_id = $this->id;
+                    $media->filename = $path;
+
+                    $metadata = '{"name":"' . $image->getClientOriginalName() . '", "extension" :"' . $image->extension() . '"}';
+                    $media->metadata = $metadata;
+
+                    $media->save();
+                }
+            }
+        }
     }
 }
