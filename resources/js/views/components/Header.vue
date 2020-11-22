e9eb14031ca527dd0dd23c3a1afd09cb16d19838
RDarkCat <m2007user@rambler.ru>
2020-11-22 22:34:57 +0300

    Пробую интегрировать верстку

--- resources/js/views/components/Header.vue
+++ resources/js/views/components/Header.vue
@@ -1,14 +1,41 @@
 <template>
-    <div>
-        <div id="logo">
-            <router-link
-                to="/home"
-            >
-                LOGO
-            </router-link>
+	<nav class="navbar navbar-expand-lg navbar-dark bg-dark sticky-top nav_bg non-transparent-nav">
+      <div class="container">
+        <a class="navbar-brand" href="#">Brand</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+              <a class="nav-link" href="#">Главная <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Link</a>
+            </li>
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                Dropdown
+              </a>
+              <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                <a class="dropdown-item" href="#">Action</a>
+                <a class="dropdown-item" href="#">Another action</a>
+                <div class="dropdown-divider"></div>
+                <a class="dropdown-item" href="#">Something else here</a>
+              </div>
+            </li>
+
+          </ul>
+          <li class="form-inline my-2 my-lg-0">
+            <a class="nav-link" href="#">Личный кабинет</a>
+          </li>
+
+            <a href="#" class="btn btn-primary login_btn">Вход</a>
+
         </div>
-        <navigation />
-    </div>
+      </div>
+    </nav>
 </template>
 
 <script>
