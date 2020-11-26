a3a73e8b4ce2fbb392353678029d55034bfa4b47
Gallaguz <gallaguz@gmail.com>
2020-11-27 01:27:52 +0300

    подключил бутстрап средствани npm

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,11 +4,17 @@ window.Vue = require('vue');
 import Axios from "axios";
 Vue.use(VueAxios, Axios);
 
+import BootstrapVue from 'bootstrap-vue';
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+Vue.use(BootstrapVue);
+
 import VueAxios from 'vue-axios';
 import store from './store';
 import router from './routes/routes';
 import App from './App.vue';
-
 
 require('./store/subscriber');
 
