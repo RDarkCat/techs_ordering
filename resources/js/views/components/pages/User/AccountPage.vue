276b6ef47002ca0b9c63260d3214272b1f8f433f
Gallaguz <gallaguz@gmail.com>
2020-11-26 19:01:50 +0300

    .

--- resources/js/views/components/pages/User/AccountPage.vue
+++ resources/js/views/components/pages/User/AccountPage.vue
@@ -1,12 +1,30 @@
 <template>
     <div>
-        Account
+        Account:
+        <div>
+            <button id="show-modal" @click="showModal = true">Create promo</button>
+            <CreatePromo v-if="showModal" @close="showModal = false">
+                <h3 slot="header">PLS fill the form</h3>
+            </CreatePromo>
+        </div>
     </div>
 </template>
 
 <script>
+import CreatePromo from "./elements/CreatePromo";
 export default {
-    name: "AccountPage"
+    name: "AccountPage",
+    components: {
+        CreatePromo
+    },
+    data () {
+        return {
+            showModal: false
+        }
+    },
+    methods: {
+
+    }
 }
 </script>
 
