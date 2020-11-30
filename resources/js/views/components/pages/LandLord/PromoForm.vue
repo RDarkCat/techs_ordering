17048bb8be26449585f27faadc20abda78536f7b
RDarkCat <m2007user@rambler.ru>
2020-11-30 18:20:07 +0300

    first

---
+++ resources/js/views/components/pages/LandLord/PromoForm.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <form>
+            <div class=" card">
+                <div class="d-flex justify-content-between card-body">
+                
+
+                    <div>
+                        <div class="form-group">
+                            <label for="exampleFormControlSelect1">Название:</label>
+                            <input type="text" id="item-name"/>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </form>
+    </div>
+</template>
+
+
+
+export default {
+    "name": "PromoForm"
+}
+
+
+<style scoped>
+</style>
