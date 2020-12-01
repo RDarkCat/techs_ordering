fd13d30d3fd1bd0a795bf41cd2fae7b4bfa99903
stfakel <stfakel11@gmail.com>
2020-12-01 14:03:21 +0300

    верстка страницы single promo

--- resources/js/views/components/pages/Promos/elements/PromoCard.vue
+++ resources/js/views/components/pages/Promos/elements/PromoCard.vue
@@ -2,9 +2,11 @@
     <div>
         <section class=" pb-5 bg-light">
             <div class="container pt-5">
-                <div class="d-flex justify-content-between">
-                    <div>
-                        <h3>Название: {{ promo.item.name }}</h3>
+                <div class="row justify-content-between">
+
+
+                    <div class="col-lg-6">
+                        <h3>C{{ promo.item.name }}</h3>
                         <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
                             <ol class="carousel-indicators">
                                 <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
@@ -13,13 +15,13 @@
                             </ol>
                             <div class="carousel-inner">
                                 <div class="carousel-item active">
-                                    <img class="d-block w-100" src="css/img/carousel/C671759.jpeg" width="200" alt="First slide">
+                                    <img class="d-block w-100" src="https://i.pinimg.com/736x/83/c3/28/83c328aecd869857da6fb579ad66820e--tonka-toys-heavy-equipment.jpg" width="200" alt="First slide">
                                 </div>
                                 <div class="carousel-item">
-                                    <img class="d-block w-100" src="css/img/carousel/C722147.jpeg" width="200" alt="Second slide">
+                                    <img class="d-block w-100" src="https://i.ytimg.com/vi/6giym5yNhlo/0.jpg" width="200" alt="Second slide">
                                 </div>
                                 <div class="carousel-item">
-                                    <img class="d-block w-100" src="css/img/carousel/C740870.jpeg" width="200" alt="Third slide">
+                                    <img class="d-block w-100" src="https://www.australiahqj.com/wp-content/uploads/2019/04/New-Cat-777G-768x455.jpg" width="200" alt="Third slide">
                                 </div>
                             </div>
                             <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
@@ -33,23 +35,17 @@
                         </div>
                     </div>
 
-
-
-                    <div class="card ml-5 mt-5">
-                        <h5 class="card-header">Продавец: "ООО Caterpillar"</h5>
-                        <div class="card-body d-flex flex-column">
-
-                            <h2>Цена <span>{{ promo.price_per_day }} руб / сут</span></h2>
-                            <a href=""><i class="far fa-envelope"></i> Написать продавцу</a>
-                            <a href=""><i class="fas fa-phone"></i> Позвонить продавцу</a>
-                            <p class=""><i class="fas fa-location-arrow"></i> Москва</p>
-
+                    <div class="col-lg-4">
+                        <div class="card mt-5">
+                            <h5 class="card-header text_dark">Продавец: "ООО Caterpillar"</h5>
+                            <div class="card-body d-flex flex-column">
+                                <h2>Цена <span>{{ promo.price_per_day }} руб / сут</span></h2>
+                                <a href=""><i class="far fa-envelope"></i> Написать продавцу</a>
+                                <a href=""><i class="fas fa-phone"></i> Позвонить продавцу</a>
+                                <p class=""><i class="fas fa-location-arrow"></i> Москва</p>
+                            </div>
                         </div>
-
                     </div>
-
-
-
 
                 </div>
                 <div class="mt-3">
@@ -63,12 +59,12 @@
                     <p>Диапазон массы кузова	20 831–30 623 кг 45 924–67 512 фунтов</p>
                     <p>Грузоподъемность: {{ promo.item.characteristic.lifting_capacity }}</p>
                     <h5>Описание</h5>
-                <p>{{ promo.item.characteristic.description }}</p>
-                            		<p>Высота: {{ promo.item.characteristic.height }}</p>
+                    <p>{{ promo.item.characteristic.description }}</p>
+                    <p>Высота: {{ promo.item.characteristic.height }}</p>
                     <p>Ширина: {{ promo.item.characteristic.width }}</p>
                     <p>Длина: {{ promo.item.characteristic.length }}</p>
                     <p>Масса: {{ promo.item.characteristic.mass }}</p>
-                Объявление создано:  {{ promo.created_at }}
+                    Объявление создано:  {{ promo.created_at }}
                 </div>
             </div>
         </section>
